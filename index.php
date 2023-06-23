b82d82d3dd9e03be635bb33aca78d537fd290a98
tomczi <tomaszpyszczek@gmail.com>
2023-06-23 18:23:03 +0200

    dzialajace logowanie

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+    <title>Notes - login</title>
+  </head>
+  <body>
+    <div class="container">
+        <div class="text">
+           Notes<br><h1>Panel logowania</h1>
+        </div>
+        <form action="login.php" method="post">
+         <?php if(isset($_GET['error'])) { ?>
+            <p class="error"><?php echo $_GET['error']; ?></p>
+         <?php } ?>
+           <div class="form-row">
+              <div class="input-data">
+                 <input type="text" name="uname" required>
+                 <div class="underline"></div>
+                 <label for="">Adres Email</label>
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="input-data">
+                 <input type="text" name="password" required>
+                 <div class="underline"></div>
+                 <label for="">Hasło</label>
+              </div>
+           </div>
+           <div class="form-row">
+           <div class="input-data textarea">
+              <div class="form-row submit-btn">
+                 <div class="input-data">
+                    <div class="inner"></div>
+                    <input type="submit" value="Zaloguj">
+                 </div>
+                 <div class="input-data">
+                    <div class="inner"></div>
+                    <input type="submit" value="Stwórz konto">
+                 </div>
+              </div>
+        </form>
+        </div>
+
+  </body>
+</html>
