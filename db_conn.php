123e60ab5b3f4c38ac0baa80aecad33a12bc252d
Szymon Tylczyński <ul0253554@edu.uni.lodz.pl>
2023-08-28 20:43:41 +0200

    zmiana bazy danych na online

--- db_conn.php
+++ db_conn.php
@@ -1,10 +1,10 @@
 <?php
 
 $sname = "localhost";
-$unmae = "root";
-$password = "";
+$unmae = "2023_szymont";
+$password = "392782";
 
-$db_name = "notatnik_db";
+$db_name = "2023_szymont";
 
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
 
